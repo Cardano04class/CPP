c3b8058f45919001f566ec11c4576a74678b730e
MyMac <raja_amir@outlook.fr>
2025-05-12 13:49:18 +0100

    m2 ex0

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+const int Fixed::fractionalBits = 8;
+
+Fixed::Fixed() : fixedPointValue(0){
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    fixedPointValue = other.fixedPointValue;
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    if (this != &other)
+        this->fixedPointValue = other.fixedPointValue;
+    std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    fixedPointValue = raw;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;    
+}
