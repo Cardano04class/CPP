3a58a6118b0dec99df5827550f83c4670efbee7e
cardano04class <raja_amir@outlook.fr>
2025-12-01 05:03:12 +0100

    cpp8/ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,25 +1,13 @@
 #pragma once
 
 #include <algorithm>
-#include <stdexcept>
 
 template <typename T>
-typename T::iterator easyfind(T &container, int value)
+bool easyfind(T &container, int value)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
-
     if (it == container.end())
-        throw std::runtime_error("Value not found in container");
-    return it;
-}
-
-template <typename T>
-typename T::const_iterator easyfind(const T &container, int value)
-{
-    typename T::const_iterator it = std::find(container.begin(), container.end(), value);
-
-    if (it == container.end())
-        throw std::runtime_error("Value not found in container");
-    return it;
+        return false;
+    return true;
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,6 @@
 
 int main()
 {
-    // Test with vector
     std::cout << "===== Testing with std::vector =====" << std::endl;
     std::vector<int> vec;
     vec.push_back(10);
@@ -17,99 +16,56 @@ int main()
     
     try
     {
-        std::vector<int>::iterator it = easyfind(vec, 30);
-        std::cout << "Value found in vector: " << *it << std::endl;
-        std::cout << "Position: " << std::distance(vec.begin(), it) << std::endl;
+        if(easyfind(vec, 30))
+            std::cout << "Value 30 found in vector " << std::endl;
+        else
+            throw std::runtime_error("Value Not found"); 
     }
     catch (const std::exception &e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-    
     try
     {
-        std::vector<int>::iterator it = easyfind(vec, 100);
-        std::cout << "Value found in vector: " << *it << std::endl;
+        if (easyfind(vec, 550))
+            std::cout << "Value 550 found in vector " << std::endl;
+        else
+            throw std::runtime_error("Value Not found");
     }
     catch (const std::exception &e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-    
-    // Test with list
-    std::cout << "\n===== Testing with std::list =====" << std::endl;
+
+    std::cout << "===== Testing with std::list =====" << std::endl;
     std::list<int> lst;
-    lst.push_back(5);
-    lst.push_back(15);
-    lst.push_back(25);
-    lst.push_back(35);
-    lst.push_back(45);
-    
+
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+    lst.push_back(40);
+
     try
     {
-        std::list<int>::iterator it = easyfind(lst, 25);
-        std::cout << "Value found in list: " << *it << std::endl;
-        std::cout << "Position: " << std::distance(lst.begin(), it) << std::endl;
+        if(easyfind(lst, 20))
+            std::cout << "20 found in list" << std::endl;
+        else
+            throw std::runtime_error("Value Not found");
     }
-    catch (const std::exception &e)
+    catch(const std::exception& e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-    
     try
     {
-        std::list<int>::iterator it = easyfind(lst, 100);
-        std::cout << "Value found in list: " << *it << std::endl;
+        if(easyfind(lst, 100))
+            std::cout << "100 found in list" << std::endl;
+        else
+            throw std::runtime_error("Value Not found");
     }
-    catch (const std::exception &e)
+    catch(const std::exception& e)
     {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
-    
-    // Test with deque
-    std::cout << "\n===== Testing with std::deque =====" << std::endl;
-    std::deque<int> deq;
-    deq.push_back(1);
-    deq.push_back(2);
-    deq.push_back(3);
-    deq.push_back(4);
-    deq.push_back(5);
-    
-    try
-    {
-        std::deque<int>::iterator it = easyfind(deq, 3);
-        std::cout << "Value found in deque: " << *it << std::endl;
-        std::cout << "Position: " << std::distance(deq.begin(), it) << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    
-    try
-    {
-        std::deque<int>::iterator it = easyfind(deq, 100);
-        std::cout << "Value found in deque: " << *it << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    
-    // Test with const container
-    std::cout << "\n===== Testing with const container =====" << std::endl;
-    const std::vector<int> constVec(vec);
-    
-    try
-    {
-        std::vector<int>::const_iterator it = easyfind(constVec, 40);
-        std::cout << "Value found in const vector: " << *it << std::endl;
-        std::cout << "Position: " << std::distance(constVec.begin(), it) << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    
     return 0;
 }
