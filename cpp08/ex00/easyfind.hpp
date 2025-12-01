634195eb517a9d548d1ebc4f341c66fda5c5c3d9
cardano04class <raja_amir@outlook.fr>
2025-12-01 23:04:10 +0100

    cpp8/ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
 #include <algorithm>
+#include <stdexcept>
 
 template <typename T>
-bool easyfind(T &container, int value)
+typename T::iterator easyfind(T &container, int value)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
     if (it == container.end())
-        return false;
-    return true;
+        throw std::runtime_error("Value not Found");
+    return it;
 }
 
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value)
+{
+    typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end())
+        throw std::runtime_error("Value not Found");
+    return it;
+}
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,68 +4,96 @@
 #include <deque>
 #include "easyfind.hpp"
 
-int main()
+int main(void)
 {
-    std::cout << "===== Testing with std::vector =====" << std::endl;
     std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(5);
     vec.push_back(10);
-    vec.push_back(20);
-    vec.push_back(30);
-    vec.push_back(40);
-    vec.push_back(50);
-    
-    try
-    {
-        if(easyfind(vec, 30))
-            std::cout << "Value 30 found in vector " << std::endl;
-        else
-            throw std::runtime_error("Value Not found"); 
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
-    try
-    {
-        if (easyfind(vec, 550))
-            std::cout << "Value 550 found in vector " << std::endl;
-        else
-            throw std::runtime_error("Value Not found");
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+    vec.push_back(42);
 
-    std::cout << "===== Testing with std::list =====" << std::endl;
     std::list<int> lst;
+    lst.push_back(100);
+    lst.push_back(200);
+    lst.push_back(300);
 
-    lst.push_back(10);
-    lst.push_back(20);
-    lst.push_back(30);
-    lst.push_back(40);
+    std::deque<int> deq;
+    deq.push_back(-1);
+    deq.push_back(-5);
+    deq.push_back(-10);
+
+    std::cout << "===== Testing std::vector =====" << std::endl;
+    try
+    {
+        std::vector<int>::iterator it = easyfind(vec, 10);
+        std::cout << "Found in vector: " << *it << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 
     try
     {
-        if(easyfind(lst, 20))
-            std::cout << "20 found in list" << std::endl;
-        else
-            throw std::runtime_error("Value Not found");
+        easyfind(vec, 999);
     }
-    catch(const std::exception& e)
+    catch (std::exception &e)
     {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cout << "Vector error: " << e.what() << std::endl;
     }
+
+    std::cout << "\n===== Testing std::list =====" << std::endl;
     try
     {
-        if(easyfind(lst, 100))
-            std::cout << "100 found in list" << std::endl;
-        else
-            throw std::runtime_error("Value Not found");
+        std::list<int>::iterator it = easyfind(lst, 200);
+        std::cout << "Found in list: " << *it << std::endl;
     }
-    catch(const std::exception& e)
+    catch (std::exception &e)
     {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cout << e.what() << std::endl;
     }
+
+    try
+    {
+        easyfind(lst, 123);
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "List error: " << e.what() << std::endl;
+    }
+
+    std::cout << "\n===== Testing std::deque =====" << std::endl;
+    try
+    {
+        std::deque<int>::iterator it = easyfind(deq, -10);
+        std::cout << "Found in deque: " << *it << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    try
+    {
+        easyfind(deq, 0);
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Deque error: " << e.what() << std::endl << std::endl;
+    }
+
+    std::cout << "===== Testing const container =====" << std::endl;
+    const std::vector<int> cvec = vec;
+
+    try
+    {
+        std::vector<int>::const_iterator it = easyfind(cvec, 42);
+        std::cout << "Found in const vector: " << *it << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
