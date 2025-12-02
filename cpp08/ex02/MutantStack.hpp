89321cdffedfd51db41915843dcc9c908dc9ed27
cardano04class <raja_amir@outlook.fr>
2025-12-02 03:44:41 +0100

    cpp8

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,39 +1,30 @@
 #pragma once
 
 #include <stack>
-#include <deque>
 
-template <typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+template <typename T>
+class MutantStack : public std::stack<T>
 {
     public:
-        MutantStack() : std::stack <T, Container>(){}
-        MutantStack(const MutantStack &other) : std::stack<T, Container>(other){}
+        MutantStack(){}
+        MutantStack(const MutantStack &other) : std::stack<T>(other){}
         ~MutantStack(){}
 
         MutantStack &operator=(const MutantStack &rhs)
         {
             if (this != &rhs)
             {
-                std::stack<T, Container>::operator=(rhs);
+                std::stack<T>::operator=(rhs);
             }
             return *this;
         }
 
-        typedef typename Container::iterator iterator;
-        typedef typename Container::const_iterator const_iterator;
-        typedef typename Container::reverse_iterator reverse_iterator;
-        typedef typename Container::const_reverse_iterator const_reverse_iterator;
-
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        
         iterator begin() { return this->c.begin();}
         iterator end() { return this->c.end();}
 
         const_iterator begin() const { return this->c.begin();}
         const_iterator end() const { return this->c.end();}
-
-        reverse_iterator rbegin() { return this->c.rbegin();}
-        reverse_iterator rend() { return this->c.rend();}
-
-        const_reverse_iterator rbegin() const { return this->c.rbegin();}
-        const_reverse_iterator rend() const { return this->c.rend();}
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -8,9 +8,9 @@ int main()
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	// std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	// std::cout << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -26,5 +26,19 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
-	return 0;
+
+	 // ===== std::list Test =====
+    std::cout << "\n=== std::list Test ===" << std::endl;
+    std::list<int> lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    lst.pop_back();
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+
+    std::cout << "Iterating std::list:" << std::endl;
+    for (std::list<int>::iterator iter = lst.begin(); iter != lst.end(); ++iter)
+        std::cout << *iter << std::endl;
 }
